3781970296e3ff18554fdbba74a3ad209505853e
Shaunak <shaunak9422@gmail.com>
2024-03-04 21:30:59 +0530

    edit 3

---
+++ src/store/CartContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useReducer } from "react";
+
+const CartContext = createContext({
+    items: [], 
+    addItem: (item) => {}, 
+    removeItem: (id) => {}
+});
+
+function cartReducer(state, action) {
+    
+}
+
+
+export function CartContextProvider({children}) {
+    useReducer();
+
+
+    return <CartContext.Provider>
+        {children}
+    </CartContext.Provider>
+}
+
+export default CartContext;
