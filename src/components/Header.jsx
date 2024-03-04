91be7f1900af60bb79f35b81f2deb10e45abf8e6
Shaunak <shaunak9422@gmail.com>
2024-03-04 22:27:28 +0530

    edit 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
         <header id="main-header">
             <div id="title">
                 <img src={logoImg} alt="A restaurant" />
-                <h1>ReactFood</h1>
+                <h1>React Food</h1>
             </div>
             <nav>
                 <Button textOnly>Cart (0)</Button>
